7493ae5999ab3583f2336663195c7b81e617cd2c
bellapalumbi <bpalumbi@youth.dhftc.org>
2018-03-26 15:00:29 -0400

    Added more sprites and blurb functionality

---
+++ Narrative Game/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionDetector : MonoBehaviour {
+
+	// Use this for initialization
+	public Text ItemText;
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		print ("Hit " + coll.gameObject.name);
+		ChangeText(coll.gameObject.name);
+	}
+
+	void ChangeText(string name) {
+		if (name == "sofa") {
+			ItemText.text = "Sofa was hit";
+		} 
+		if (name == "shelf") {
+			ItemText.text = "Shelf was hit";
+		}
+		if (name == "door") {
+			ItemText.text = "Door was hit";
+		}
+		if (name == "lamp") {
+			ItemText.text = "Lamp was hit";
+		}
+
+	}
+
+}
