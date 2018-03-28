f65837e8fb7db071a07a880a5a94f35a855ca61a
bellapalumbi <bpalumbi@youth.dhftc.org>
2018-03-28 09:26:06 -0400

    Added TV sprites and switching functionality

--- Narrative Game/Assets/Scripts/CollisionDetector.cs
+++ Narrative Game/Assets/Scripts/CollisionDetector.cs
@@ -7,24 +7,53 @@ public class CollisionDetector : MonoBehaviour {
 
 	// Use this for initialization
 	public Text ItemText;
+	
+	SwitchPlayer switchPlayer;
+
+	void Start() {
+		switchPlayer = gameObject.GetComponent<SwitchPlayer>();
+		
+	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		print ("Hit " + coll.gameObject.name);
 		ChangeText(coll.gameObject.name);
 	}
 
+
 	void ChangeText(string name) {
-		if (name == "sofa") {
-			ItemText.text = "Sofa was hit";
-		} 
-		if (name == "shelf") {
-			ItemText.text = "Shelf was hit";
-		}
-		if (name == "door") {
-			ItemText.text = "Door was hit";
-		}
-		if (name == "lamp") {
-			ItemText.text = "Lamp was hit";
+		if(switchPlayer.isToddler) {
+			if (name == "sofa") {
+				ItemText.text = "Over here’s the waterfall. I like to jump up all the way to the tippy top and then slide down. Sometimes Mr. Whiskers sits on the top of the waterfall, and one time he fell off and ran away.";
+			} 
+			if (name == "shelf") {
+				ItemText.text = "This is like a rainbow right in our house. It has books of all different colors and they go all the way up to the sky. You can take one of the books off and it has an entire story inside. The ones on the bottom shelf are all pictures and my mom likes to read me them before I go to bed.";
+			}
+			if (name == "door") {
+				ItemText.text = "Door was hit";
+			}
+			if (name == "lamp") {
+				ItemText.text = "Lamp was hit";
+			}
+			if (name == "tv") {
+				ItemText.text = "TV was hit";
+			}
+		} else if (!switchPlayer.isToddler) {
+			if (name == "sofa") {
+				ItemText.text = "Sofa was hit -2";
+			} 
+			if (name == "shelf") {
+				ItemText.text = "Shelf was hit2";
+			}
+			if (name == "door") {
+				ItemText.text = "Door was hit2";
+			}
+			if (name == "lamp") {
+				ItemText.text = "Lamp was hit2";
+			}
+			if (name == "tv") {
+				ItemText.text = "TV was hit2";
+			}
 		}
 
 	}
---
+++ Narrative Game/Assets/Scripts/SwitchPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPlayer : MonoBehaviour {
+
+	SpriteRenderer spriteRenderer;
+	public bool isToddler;
+	// Use this for initialization
+	void Start () {
+		isToddler = true;
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+			print("space was pressed");
+			if (isToddler) {
+				isToddler = false;
+				spriteRenderer.color = Color.cyan;
+			} else if (!isToddler) { // so, is adult
+				isToddler = true;
+				spriteRenderer.color = Color.white;
+			}
+			print(isToddler);
+		}
+	}
+}
